7d7da42a6a0b60cd8d95d098b59ad39060d840cd
Tien Phan <tienphan1989@gmail.com>
2020-05-07 08:34:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ input = gets.strip
 songs.each_with_index do |song, index| 
   if input == song |
     puts "Playing #{song}"
-    elsif index == input[index-1]
+    elsif song == input[index-1]
     puts "Playing #{song}"
   else 
     puts "Invalid input, please try again"
