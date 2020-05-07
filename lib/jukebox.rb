3f9c30f390ac86e14fd69473b976242c2010e2e0
Tien Phan <tienphan1989@gmail.com>
2020-05-07 09:04:10 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,62 @@
-# Add your code here
+require 'pry'
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+puts "I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+end
+
+def list(songs)
+songs.each_with_index do |song, index| 
+  puts "#{index+1}. #{song}"
+end
+end
+
+def play(songs)
+puts "Please enter a song name or number:"
+input = gets.chomp
+if (1..9).to_a.include?(input.to_i)
+    puts "Playing #{songs[input.to_i - 1]}"
+    elsif songs.include?(input)
+    puts "Playing #{input}"
+  else 
+    puts "Invalid input, please try again"
+  end 
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+puts "Please enter a command:"
+input = gets.chomp
+until input == exit
+if input == help
+  help
+  run
+  if input == list
+    list(songs)
+    run
+    if input == play
+      play(songs)
+      run
+    end
+  end
+end
+end
+end
