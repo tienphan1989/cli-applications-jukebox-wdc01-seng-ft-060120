53260a30359e78b7627808ceca9972e748c0840d
Tien Phan <tienphan1989@gmail.com>
2020-05-07 08:50:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,15 +29,13 @@ end
 def play(songs)
 puts "Please enter a song name or number:"
 input = gets.chomp
-songs.each_with_index do |song, index| 
-  if input.to_i == index 
-    puts "Playing #{song[index-1]}"
-    elsif input == song
-    puts "Playing #{song[index]}"
+if (1..9).to_a.include?(input.to_i)
+    puts "Playing #{songs[input.to_i - 1]}"
+    elsif songs.include?(input)
+    puts "Playing #{input}"
   else 
     puts "Invalid input, please try again"
-  end
-end
+  end 
 end
 
 def exit_jukebox
